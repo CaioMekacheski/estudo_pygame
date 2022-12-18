479f001bad4aa1db2ff6e8764cc4193459421cdf
Caio Cesar Mekacheski de Araújo <106687263+CaioMekacheski@users.noreply.github.com>
2022-12-17 21:41:30 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,10 +1,17 @@
+# Atualizado em 17/12/2022
+
 import math
+import numpy as np
 
 RES = WIDTH, HEIGHT = 800, 600
 HALF_WIDTH = WIDTH // 2
 HALF_HEIGHT = HEIGHT // 2
+HOR_RES = 160
+HALF_VER_RES = 120
+MOD = HOR_RES / 60
+
 PLAYER_POSX = 1.5
-PLAYER_POSY = 5
-PLAYER_ANGLE = 0
+PLAYER_POSY = np.random.randint(1, SIZE - 1)
+PLAYER_ANGLE = np.pi / 4
 PLAYER_SPEED = 0.002
 PLAYER_ROT_SPEED = 0.001
