4427306c2ede6b97d14865709bedb69844bf7cce
Caio Cesar Mekacheski de Araújo <106687263+CaioMekacheski@users.noreply.github.com>
2022-12-17 21:32:39 -0300

    Update estudo_ray_casting.py

--- estudo_ray_casting.py
+++ estudo_ray_casting.py
@@ -1,29 +1,32 @@
+# Atualizado 17/12/2022
+# Encontre a saída do labirinto 
+
 import pygame as pg
-import numpy as np
 from numba import njit
 from settings import *
 
 
 def main():
     pg.init()
-    screen = pg.display.set_mode(RES)
+    screen = pg.display.set_mode(RESOLUTION)
     running = True
     clock = pg.time.Clock()
 
-    hres = HALF_WIDTH  # Resolução horizontal
-    halfvres = HALF_HEIGHT  # Resolução vertical / 2
+    hres = HOR_RES  # Resolução horizontal
+    halfvres = HALF_VER_RES  # Resolução vertical / 2
 
-    mod = hres/60   # Fator de escala (60º fov)
-    posx, posy, rot = PLAYER_POSX, PLAYER_POSX, PLAYER_ANGLE   # Posição x, y e rotação
+    mod = MOD  # Fator de escala (60º fov)
 
-    size = 25
-    maph = np.random.choice([0, 0, 0, 1], (size, size))
-    mapc = np.random.uniform(0, 1, (size, size, 3))
+    # Mapa
+    size = SIZE
+    posx, posy, rot, maph, mapc, exitx, exity = gen_map(size)
 
     frame = np.random.uniform(0, 1, (hres, halfvres * 2, 3))    # Frame
 
     # Carrega e armazena a imagem do céu em um array 3d
-    sky = pg.surfarray.array3d(pg.transform.scale(pg.image.load('skybox2.jpg'), (360, halfvres * 2)))/255
+    image_width = 360
+    sky = pg.surfarray.array3d(pg.transform.scale
+                               (pg.image.load('skybox2.jpg'), (image_width, halfvres * 2)))/255
 
     # Carrega e armazena a imagem do chão em um array 3d
     floor = pg.surfarray.array3d(pg.image.load('floor04.png'))/255
@@ -32,31 +35,45 @@ def main():
     wall = pg.surfarray.array3d(pg.image.load('wall01.jpg'))/255
 
     while running:  # Enquanto running for true
-        for event in pg.event.get():    # Checa eventos
-            if event.type == pg.QUIT:   # Se o evento for QUIT
-                running = False     # Encerra o programa
 
-        # Floor casting
+        for event in pg.event.get():    # Checa eventos
+
+            if event.type == pg.QUIT:   # Se o evento for QUIT
+                running = False     # encerra o jogo
+
+            if int(posx) == exitx and int(posy) == exity:   # Se o player encontrar a saída
+                print("Você encontrou a saída! Parabéns!!") # encerras o jogo
+                running = False
+
+
+        # Cria o frame
         frame = new_frame(posx, posy, rot,
                           frame, sky, floor,
                           hres, halfvres, mod,
-                          maph, mapc, size, wall)
+                          maph, mapc, size, wall,
+                          exitx, exity)
 
+        # Cria uma superfície para receber o frame e o adapta conforme a altura e largura da tela
         surf = pg.surfarray.make_surface(frame * 255)
-        surf = pg.transform.scale(surf, RES)
+        surf = pg.transform.scale(surf, RESOLUTION)
 
+        # Imprime o fps e a posição x e y na borda superior da janela
         fps = int(clock.get_fps())
         pg.display.set_caption(" FPS: " + str(fps) +
-                               " X: " + str(posx) +
-                               " Y: " + str(posy) +
-                               " Rotação: " + str(rot))
+                               " X: " + str(int(posx)) +
+                               " Y: " + str(int(posy)) +
+                               " Rotação: " + str(int(rot)))
 
-        posx, posy, rot = movement(posx, posy, rot, pg.key.get_pressed(), clock.tick())
+        # Define as cordenadas x e y e a rotação do player
+        posx, posy, rot = movement(posx, posy, rot, pg.key.get_pressed(), maph, clock.tick())
         screen.blit(surf, (0, 0))
         pg.display.update()
-        posx, posy, rot = movement(posx, posy, rot, pg.key.get_pressed(), clock.tick())
+        #posx, posy, rot = movement(posx, posy, rot, pg.key.get_pressed(), maph, clock.tick())
 
-def movement(posx, posy, rot, keys, et):  # Movimentação do player
+# Movimento do player
+def movement(posx, posy, rot, keys, maph, et):  # Movimentação do player
+
+    x, y, diag = posx, posy, rot
 
     if keys[pg.K_LEFT] or keys[ord('a')]:  # Rotaciona a camera para a direita
         rot -= PLAYER_ROT_SPEED * et
@@ -65,16 +82,65 @@ def movement(posx, posy, rot, keys, et):  # Movimentação do player
         rot += PLAYER_ROT_SPEED * et
 
     if keys[pg.K_UP] or keys[ord('w')]:     # Move para frente
-        posx, posy = posx + np.cos(rot) * PLAYER_SPEED * et, posy + np.sin(rot) * PLAYER_SPEED * et
+        x, y, diag = posx + np.cos(rot) * PLAYER_SPEED * et, \
+                     posy + np.sin(rot) * PLAYER_SPEED * et, 1
 
     if keys[pg.K_DOWN] or keys[ord('s')]:   # Move para trás
-        posx, posy = posx - np.cos(rot) * PLAYER_SPEED * et, posy - np.sin(rot) * PLAYER_SPEED * et
+        x, y, diag = posx - np.cos(rot) * PLAYER_SPEED * et, \
+                     posy - np.sin(rot) * PLAYER_SPEED * et, 1
+
+    # Verifica se há paredes no caminho
+    if not (maph[int(x - 0.1)][int(y)] or maph[int(x + 0.1)][int(y)] or
+            maph[int(x)][int(y - 0.1)] or maph[int(x)][int(y + 0.1)]):
+        posx, posy = x, y
+
+    elif not (maph[int(posx - 0.1)][int(y)] or maph[int(posx + 0.1)][int(y)] or
+              maph[int(posx)][int(y - 0.1)] or maph[int(posx)][int(y + 0.1)]):
+        posy = y
+
+    elif not (maph[int(x - 0.1)][int(posy)] or maph[int(x + 0.1)][int(posy)] or
+              maph[int(x)][int(posy - 0.1)] or maph[int(x)][int(posy + 0.1)]):
+        posx = x
 
     return posx, posy, rot
 
+# Criação do mapa
+def gen_map(size):
+    mapc = np.random.uniform(0, 1, (size, size, 3))
+    maph = np.random.choice([0, 0, 0, 0, 1, 1], (size, size))
+    maph[0, :], maph[size - 1, :], maph[:, 0], maph[:, size - 1] = (1, 1, 1, 1)
 
+    # Define as coordendas
+    posx, posy, rot = 1.5, np.random.randint(1, size - 1) + .5, np.pi / 4
+    x, y = int(posx), int(posy)
+    maph[x][y] = 0
+    count = 0
+
+    # Define a posição da saída no mapa
+    while True:
+        testx, testy = (x, y)
+        if np.random.uniform() > 0.5:
+            testx = testx + np.random.choice([-1, 1])
+        else:
+            testy = testy + np.random.choice([-1, 1])
+
+        if testx > 0 and testx < size - 1 and testy > 0 and testy < size - 1:
+
+            if maph[testx][testy] == 0 or count > 5:
+                count = 0
+                x, y = (testx, testy)
+                maph[x][y] = 0
+
+                if x == size - 2:
+                    exitx, exity = (x, y)
+                    break
+            else:
+                count = count + 1
+    return posx, posy, rot, maph, mapc, exitx, exity
+
+# Criação do frame
 @njit()
-def new_frame(posx, posy, rot, frame, sky, floor, hres, halfvres, mod, maph, mapc, size, wall):
+def new_frame(posx, posy, rot, frame, sky, floor, hres, halfvres, mod, maph, mapc, size, wall, exitx, exity):
 
     for i in range(hres):  # Percorre todas as colunas da tela
 
@@ -106,11 +172,24 @@ def new_frame(posx, posy, rot, frame, sky, floor, hres, halfvres, mod, maph, map
         if shade > 1:
             shade = 1
 
+        ash = 0
+
+        if maph[int(x - 0.33) % (size - 1)][int(y - 0.33) % (size - 1)]:
+            ash = 1
+
+
+        if maph[int(x - 0.01) % (size - 1)][int(y - 0.01) % (size - 1)]:
+            shade, ash = shade * 0.5, 0
+
         c = shade * mapc[int(x) % (size - 1)][int(y) % (size - 1)]
 
         for k in range(h * 2):
 
             if halfvres - h + k >= 0 and halfvres - h + k < 2 * halfvres:
+
+                if ash and 1 - k / (2 * h) < 1 - xx / 225:
+                    c, ash = 0.5 * c, 0
+
                 frame[i][halfvres - h + k] = c * wall[xx][int(yy[k])]
 
                 if halfvres + 3 * h - k < 2 * halfvres:
@@ -124,8 +203,21 @@ def new_frame(posx, posy, rot, frame, sky, floor, hres, halfvres, mod, maph, map
 
             shade = 0.2 + 0.8 * (1 - j / halfvres)  # Define o sombreamento do chão
 
+            if maph[int(x - 0.33) % (size - 1)][int(y - 0.33) % (size - 1)]:
+                shade = shade * 0.5
+
+            elif maph[int(x - 0.33) % (size - 1)][int(y) % (size - 1)] \
+                    or maph[int(x) % (size - 1)][int(y - 0.33) % (size - 1)]:
+
+                shade = shade * 0.5
+
+
             frame[i][halfvres * 2 - j - 1] = \
                  shade * (floor[xx][yy] + frame[i][halfvres * 2 - j - 1]) / 2  # Preenche o frame considerando o sombreamento
+
+            if int(x) == exitx and int(y) == exity and (x % 1 - 0.5) ** 2 + (y % 1 - 0.5) ** 2 < 0.2:
+                ee = j / (10 * halfvres)
+                frame[i][j:2 * halfvres - j] = (ee * np.ones(3) + frame[i][j:2 * halfvres - j]) / (1 + ee)
 
     return frame
 
